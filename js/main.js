d2d3362c68a607f3232f0f3ef60359f237e85c73
Ruslan <fiesta-tm@mail.ru>
2023-02-14 22:39:12 +1000

    slaiderplus

--- js/main.js
+++ js/main.js
@@ -26,3 +26,41 @@ showTabContent(i);
 
 hideTabContent();
 showTabContent(0);
+
+
+// Slaider
+
+const previousSlaid = document.querySelector('.slaider_arrow_left'),
+      nextSlaid = document.querySelector('.slaider_arrow_right'),
+      card = document.querySelectorAll('.slaider_card'),
+      cardInfo = document.querySelectorAll('.about_board');
+     
+      let indexId=1;
+      showCard(indexId);
+      function hideCard(){
+        cardInfo.forEach(item=>{
+item.style.display="none";
+        });
+        card.forEach((item)=>{
+item.style.display='none';
+        });
+      }
+      function showCard(i){
+       
+        if(i > card.length){indexId=1;}
+        else if (i<1){indexId=card.length;}
+        hideCard();
+        card[indexId-1].style.display="block";
+        cardInfo[indexId-1].style.display="block";
+      }
+      function plusSlaid(n){
+showCard(indexId+=n);
+      }
+      previousSlaid.addEventListener('click',()=>{
+plusSlaid(-1);
+      });
+      nextSlaid.addEventListener('click',()=>{
+        plusSlaid(+1);
+      });
+    //   --------------------------
+ 
